e8c316012593e1d315780df3fcb5138d16c3720a
pb7932 <pebe1928@gmail.com>
2021-07-08 12:27:11 +0200

    Adding zaposlenici_zadaci controller

---
+++ Controllers/bank/zaposlenici_zadaci/ZaposleniciZadaciController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using bank.model.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankApi.controllers
+{
+    public partial class MyDataController : MyBaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<ZZListResponseDTO>> GetZaposleniciZadaci()
+        {
+            try
+            {
+                var rez = await _dataRepo.ZZList();
+
+                return Ok(rez);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ZZResponseDTO>> GetZaposleniciZadaciById(int id)
+        {
+            try
+            {
+                var rez = await _dataRepo.ZZGetById(id);
+
+                return Ok(rez);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponseDto>> ZaposlenikZadatakSave(ZZRequestDTO zz)
+        {
+            try
+            {
+                var res = await _dataRepo.ZZSave(zz);
+                return Ok(res);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+    }
+}
