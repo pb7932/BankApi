db3e58d81272e8629aacef36e3a74ca5dc56976c
pb7932 <pebe1928@gmail.com>
2021-07-12 16:43:21 +0200

    Adding transakcije controller

---
+++ Controllers/bank/transakcije/TransakcijeController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using bank.model.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankApi.controllers
+{
+    public partial class MyDataController : MyBaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<TransakcijaListResponseDTO>> GetTransakcije()
+        {
+            try
+            {
+                var rez = await _dataRepo.TransakcijaList();
+
+                return Ok(rez);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TransakcijaResponseDTO>> GetTransakcijeById(int id)
+        {
+            try
+            {
+                var rez = await _dataRepo.TransakcijaGetById(id);
+
+                return Ok(rez);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponseDto>> TransakcijaSave(TransakcijaRequestDTO transakcija)
+        {
+            try
+            {
+                var res = await _dataRepo.TransakcijaSave(transakcija);
+                return Ok(res);
+            }
+            finally
+            {
+                await _dataRepo.ConnectionClose();
+            }
+        }
+    }
+}
